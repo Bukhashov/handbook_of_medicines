1925a081d49659db85efdaa17180ae0b950b443e
Bukhashov <bukhashov2301@mail.ru>
2023-05-05 00:10:09 +0600

    feat: add

--- mobile/src/screen/pharmaciesScreen.js
+++ mobile/src/screen/pharmaciesScreen.js
@@ -1,37 +1,35 @@
 import React, {useState} from 'react';
-import { View, Dimensions } from "react-native";
+import { View, Text, Dimensions } from "react-native";
+import * as Location from 'expo-location'
+import * as Permissions from 'expo-permissions'
 import MapView ,{Marker, PROVIDER_GOOGLE} from 'react-native-maps';
-import * as Location from 'expo-location';
 import { useFocusEffect } from '@react-navigation/native';
 
+const { width, height } = Dimensions.get("window");
+
 const PharmaciesScreen = ({navigation}) => {
-    const { width, height } = Dimensions.get("window");
-    
     const ASPECT_RATIO = width / height;
     const LATITUDE_DELETA = 0.09;
     const LONGITUDE_DELTA = LATITUDE_DELETA * ASPECT_RATIO;
 
-    const [mapRegion, setMapRegion] = useState({
-        latitude: 49.8057253,
-        longitude: 73.077113,
-        latitudeDelta: LATITUDE_DELETA,
-        longitudeDelta: LONGITUDE_DELTA,
-    })
-
+    const [ locationResult, setLocation ] = useState( null )
+    const [ mapRegion, setRegion ] = useState( null )
+    const [ hasLocationPermissions, setLocationPermission ] = useState( false )
+    
     const getLocation = async () => {
         let { status } = await Location.requestForegroundPermissionsAsync();
 
-        if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-            return;
+        if ( 'granted' !== status ) {
+            setLocation( 'Permission to access location was denied' )
+        } else {
+            setLocationPermission( true );
         }
-        let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
-        setMapRegion({
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0922,
-        })
+
+        let { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({})
+        setLocation( JSON.stringify( { latitude, longitude } ) )
+        
+        // Center the map on the location we just fetched.
+        setRegion( { latitude, longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 } );
     }
 
     useFocusEffect(
@@ -40,16 +38,36 @@ const PharmaciesScreen = ({navigation}) => {
         }, [])
     )
 
+    if ( locationResult === null ) {
+        return <Text>Finding your current location...</Text>
+    }
+
+    if ( hasLocationPermissions === false ) {
+        return <Text>Location permissions are not granted.</Text>
+    }
+
+    if ( mapRegion === null ) {
+        return <Text>Map region doesn't exist.</Text>
+    }
+
     return (
-        <View>
-            <MapView
-              style={{width: '100%', height: '100%', backgroundColor: '#0000',}} 
-              provider={PROVIDER_GOOGLE}
-              region={mapRegion}
-              >
-                <Marker coordinate={mapRegion} title="marker"/>
-            </MapView>
-        </View>
+        <MapView
+                style={{flex: 1}}
+                region={ mapRegion }
+                initialRegion={{
+                    "latitude": 39.97343096953564,
+                    "latitudeDelta": 0.0922,
+                    "longitude": -75.12520805829233,
+                    "longitudeDelta": 0.0421,
+                }}
+                onRegionChange={ region => setRegion( region )}
+        >
+                <Marker
+                    title="YIKES, Inc."
+                    description="Web Design and Development"
+                    coordinate={{"latitude":39.969183,"longitude":-75.133308}}
+                />
+        </MapView>
     )
 }
 
